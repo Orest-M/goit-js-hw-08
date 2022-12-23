e2db75f4eccbce2ecc4e19e7d4cc2768621e920e
Orest-M <orestmikhal@gmail.com>
2022-12-23 18:11:07 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,14 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const currentTime = player.on('timeupdate', () => {});
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('time', data.seconds);
+  }, 1000)
+);
 
-player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {
-    localStorage.setItem(
-      'videoplayer-current-time',
-      Math.round(JSON.stringify(seconds))
-    );
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
-
-// localStorage.setItem('videoplayer-current-time', JSON.stringify('user'));
-// const response = localStorage.getItem('videoplayer-current-time');
-
-// console.log(JSON.parse(response));
+player.setCurrentTime(localStorage.getItem('time') || 0);
